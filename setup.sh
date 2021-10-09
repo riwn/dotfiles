2dac732bdfee7414807d9671c37ad0a0fc8d89ec
あーる <>
2021-10-09 17:50:45 +0900

    First Commit

---
+++ setup.sh
@@ -0,0 +1,29 @@
+# #!/bin/zsh
+
+# DOT_FILES=(.*)
+
+# for file in ${DOT_FILES[@]}; do
+#     ln -s $HOME/dotfiles/$file $HOME/$file
+# done
+
+# 未定義な変数があったら途中で終了する
+set -u
+
+# 今のディレクトリ
+# dotfilesディレクトリに移動する
+BASEDIR=$(dirname $0)
+cd $BASEDIR
+
+# dotfilesディレクトリにある、ドットから始まり2文字以上の名前のファイルに対して
+for f in z??*; do
+    [ "$f" = ".git" ] && continue
+    [ "$f" = ".gitconfig.local.template" ] && continue
+    [ "$f" = ".gitmodules" ] && continue
+
+    # シンボリックリンクを貼る
+    ln -snfv ${PWD}/"$f" ~/."$f"
+done
+
+ln -snfv ${PWD}/vimrc ~/.vimrc
+ln -snfv ${PWD}/.p10k.zsh ~
+ln -snfv ${PWD}/modules/prezto ~/.zprezto
